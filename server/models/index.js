b0ee6f5d69dc28d6894a26432e3b1733dc84c3e7
Cesar Roman <cesardroman18@gmail.com>
2020-07-28 18:50:47 -0400

    changed yml file

--- server/models/index.js
+++ server/models/index.js
@@ -2,10 +2,10 @@ require('dotenv').config();
 
 const { Pool } = require('pg');
 const pool = new Pool({
-  user: process.env.POSTGRES_USER,
-  host: process.env.DB_HOST,
-  database: process.env.POSTGRES_DB,
-  password: process.env.POSTGRES_PASSWORD,
+  user: 'me',
+  host: 'db',
+  database: 'api2',
+  password: 'password',
   port: 5432,
 });
 
