4642df1deca5210e6e595e09bc93db3705c0602f
Cesar Roman <cesardroman18@gmail.com>
2020-07-28 19:38:12 -0400

    correctly changed database for postgres deployment

--- server/models/index.js
+++ server/models/index.js
@@ -4,7 +4,7 @@ const { Pool } = require('pg');
 const pool = new Pool({
   user: 'me',
   host: '18.219.239.209',
-  database: 'db',
+  database: 'api2',
   password: 'password',
   port: 5432,
 });
