e890feff3ad2d46d8f37093671477733638a3952
Cesar Roman <cesardroman18@gmail.com>
2020-07-28 21:43:26 -0400

    used path to join to loader file

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const Router = require('./routes/routes.js');
+const path = require('path');
 const port = process.env.PORT;
 
 app.use(morgan('dev'));
@@ -11,7 +12,11 @@ app.use(bodyParser.json());
 
 app.get('/loaderio-a39c596e737aac30cea275cb33dea677', function (req, res) {
   res.sendFile(
-    path.join(__dirname, 'loaderio-a39c596e737aac30cea275cb33dea677.txt')
+    path.join(
+      __dirname,
+      '../server',
+      'loaderio-a39c596e737aac30cea275cb33dea677.txt'
+    )
   );
 });
 
